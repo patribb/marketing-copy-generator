83673496c739fa67d60d87d8f587d6da7a89d24a
Patricia Garcia <martinez171214@gmail.com>
2022-10-12 18:18:00 +0200

    done

---
+++ pages/api/marketing-copy.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { openai } from '../../utils/constants';
+
+type Data = {
+  input: string
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { input } = req.body;
+  const response = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: `Usted es un experto en marketing y un cliente se acerca a usted para escribir una copia de marketing breve y muy interesante para él o ella. Este es el tema que les gustaría una copia de marketing sobre el tema de '${input}.'\n\nEsta es la breve copia de marketing que se te ocurrió:`,
+    max_tokens: 200,
+    temperature: 0.85,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0
+  });
+  const suggestion = response.data?.choices?.[0].text;
+  if(suggestion === undefined) {
+    throw new Error('No se encontraron sugerencias')
+  }
+  res.status(200).json(suggestion);
+}
